425e5f9a1492b79fd0dbc1d0158dcab251625e58
yuki_s <saitoyukisaito@gmail.com>
2021-04-25 16:20:24 -0700

    init

---
+++ src/main/java/com/stocktrading/httpserver/HttpServer.java
@@ -0,0 +1,16 @@
+package com.stocktrading.httpserver;
+
+/**
+ *
+ * Driver Class for Http Server
+ *
+ */
+
+public class HttpServer {
+    public static void main(String[] args) {
+
+        System.out.println("Server starting...");
+
+    }
+
+}
